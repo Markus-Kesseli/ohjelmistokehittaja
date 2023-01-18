8f8da7c61123ffddfa7230c813ae9fbf908bbf84
Markus Kesseli <markus.kesseli@gmail.com>
2023-01-18 16:47:44 +0200

    6.0

---
+++ projekti1/5suurinta/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x, y, z, i, j;
+
+        Console.Write("Anna ensimmäinen numero: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Anna toinen numero: ");
+        y = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Anna kolmas numero: ");
+        z = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Anna neljäs numero: ");
+        i = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Anna viides numero: ");
+        j = Convert.ToInt32(Console.ReadLine());
+
+        
+
+    }
+}
---
+++ projekti1/lukusanaksi/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int x;
+        Console.Write("Anna numero 0-9 väliltä: ");
+        x = Convert.ToInt32(Console.ReadLine());
+
+        switch (x) 
+        {
+            case 0: Console.WriteLine("nolla");
+                break;
+
+            case 1: Console.WriteLine("yksi");
+                break;
+
+            case 2: Console.WriteLine("kaksi");
+                break;
+
+            case 3: Console.WriteLine("kolme");
+                break;
+
+            case 4: Console.WriteLine("neljä");
+                break;
+
+            case 5: Console.WriteLine("viisi");
+                break;
+
+            case 6: Console.WriteLine("kuusi");
+                break;
+
+            case 7: Console.WriteLine("seitsemän");
+                break;
+
+            case 8: Console.WriteLine("kahdeksan");
+                break;
+
+            case 9: Console.WriteLine("yhdeksän");
+                break;
+
+            default:
+                Console.WriteLine("Anna luku 0-9 väliltä!");
+                break;
+
+
+
+
+        }
+
+    }
+}
